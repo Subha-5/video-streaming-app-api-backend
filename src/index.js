14ebc0ee8be77d665a370f50db6f7df815d54463
Subha Sadhu <subhasadhu5@gmail.com>
2023-11-05 01:41:25 +0530

    DB connection

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db";
+
+dotenv.config({
+   path: "./env" 
+});
+
+connectDB()
+
+
+/* 
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw error;
+  }
+});
+*/
