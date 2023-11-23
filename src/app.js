d85c2d5612066c5d765a04a00bc40c322886d1b4
Subha Sadhu <subhasadhu5@gmail.com>
2023-11-24 02:48:21 +0530

    using routers with controllers

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/user", userRouter);
+// http://localhost:80/api/v1/user/....
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
-import connectDB from "./db";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -7,10 +8,10 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    app.on("error", (err) => {
-      console.log("Error thrown on connecting to database: ", err);
-      throw err;
-    });
+    // app.on("error", (err) => {
+    //   console.log("Error thrown on connecting to database: ", err);
+    //   throw err;
+    // });
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at port: ${process.env.PORT}`);
     });
